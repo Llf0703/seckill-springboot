72eccf2afb604383f396a3c987fa5435225b435c
Wky1742095859 <1742095859@qq.com>
2022-03-18 23:44:37 +0800

    初始化项目

---
+++ src/main/java/com/example/seckill_manager/SeckillManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.seckill_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seckill_manager/SeckillManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seckill_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
