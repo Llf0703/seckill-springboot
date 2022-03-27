43c3d6eae1729bcc4dc62e856db4914067b0e52e
Wky1742095859 <1742095859@qq.com>
2022-03-28 01:57:01 +0800

    决策引擎dto

---
+++ seckill_manager/src/main/java/com/seckill/seckill_manager/controller/dto/RiskControlDTO.java
@@ -0,0 +1,11 @@
+package com.seckill.seckill_manager.controller.dto;
+
+/**
+ * @author Wky1742095859
+ * @version 1.0
+ * @ClassName RiskControlDTO
+ * @description: 决策引擎entity转前端数据
+ * @date 2022/3/28 1:56
+ */
+public class RiskControlDTO {
+}
